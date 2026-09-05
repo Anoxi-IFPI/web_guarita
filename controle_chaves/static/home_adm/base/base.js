64f58d4799e58856ff71d5f9c3b820cd94d16d01
Antonio José <antonioalves073d@gamial.com>
2026-09-04 23:21:08 -0300

    ajuste de resposividade de tela

---
+++ controle_chaves/static/home_adm/base/base.js
@@ -0,0 +1,4 @@
+        document.getElementById('toggle-sidebar').addEventListener('click', function() {
+            document.getElementById('sidebar').classList.toggle('active');
+            document.getElementById('content').classList.toggle('active');
+        });
